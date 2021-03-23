c8387ae9b4e2da8fdba5b94b659997fc56268650
atul <atul@wellnite.com>
2021-03-23 13:53:38 +0530

    ...

--- src/features/Posts/Posts.tsx
+++ src/features/Posts/Posts.tsx
@@ -9,8 +9,9 @@ import {
   Title,
   Body,
   Content,
+  View,
 } from 'native-base';
-// import axios from 'axios';
+import axios from 'axios';
 
 const Status = {
   idle: 'idle',
@@ -18,59 +19,63 @@ const Status = {
   rejected: 'rejected',
   resolved: 'resolved',
 };
+const nasaApiKey = 'hLRfeCa4PCLoFSWyMsGh49F2IREUdpUl0zbnhz6w';
 
 type Props = {
   navigation: any;
 };
 
 export const Posts: FC<Props> = ({navigation}) => {
-  // const [posts, setPosts] = useState<any>([]);
+  const [posts, setPosts] = useState<any>([]);
   const [status, setStatus] = useState(Status.idle);
   const [id, setId] = useState('');
 
-  // const nasaApiKey = 'hLRfeCa4PCLoFSWyMsGh49F2IREUdpUl0zbnhz6w';
+  const apiCall = async () => {
+    console.log('api called for page no #');
+    setStatus(Status.pending);
+    try {
+      const res = await axios.get(
+        `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${nasaApiKey}`,
+      );
 
-  // const apiCall = async () => {
-  //   console.log('api called for page no #');
-  //   setStatus(Status.pending);
-  //   try {
-  //     const res = await axios.get(
-  //       `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${nasaApiKey}`,
-  //     );
-
-  //     if (res?.data) {
-  //       // console.log(
-  //       //   'response got for page no #',
-  //       //   res.data.near_earth_objects[0].id,
-  //       //   res.data.near_earth_objects.length,
-  //       // );
-  //       setPosts(res.data.near_earth_objects);
-  //       setStatus(Status.resolved);
-  //     }
-  //   } catch (error) {
-  //     // console.log('err', error);
-  //     setStatus(Status.rejected);
-  //   }
-  // };
+      if (res?.data) {
+        // console.log(
+        //   'response got for page no #',
+        //   res.data.near_earth_objects[0].id,
+        //   res.data.near_earth_objects.length,
+        // );
+        setPosts(res.data.near_earth_objects);
+        setStatus(Status.resolved);
+      }
+    } catch (error) {
+      // console.log('err', error);
+      setStatus(Status.rejected);
+    }
+  };
 
   const navigate = () => {
     navigation.navigate('PostDetails', {id: id});
   };
 
-  // useEffect(() => {
-  //   let isMount = true;
-  //   if (isMount) apiCall();
-  //   return () => {
-  //     isMount = false;
-  //   };
-  // }, []);
+  React.useEffect(() => {
+    let isMount = true;
+    if (isMount) apiCall();
+    return () => {
+      isMount = false;
+    };
+  }, []);
 
   React.useEffect(() => {
     return () => {
       setId('');
     };
   }, []);
+  //
 
+  const randomAsteroidDetails = () => {
+    const id = posts[Math.floor(Math.random() * posts.length)]?.id;
+    navigation.navigate('PostDetails', {id: id});
+  };
   return (
     <Container>
       <Header>
@@ -81,8 +86,7 @@ export const Posts: FC<Props> = ({navigation}) => {
       <Content
         contentContainerStyle={{
           justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'row',
+          // flexDirection: 'row',
           paddingTop: 10,
           paddingHorizontal: 5,
         }}>
@@ -91,9 +95,23 @@ export const Posts: FC<Props> = ({navigation}) => {
           onChangeText={(e: string) => setId(e)}
           value={id}
         />
-        <Button onPress={navigate} light={!id}>
-          <Text style={{color: id ? '#fff' : '#ccc'}}>Submit</Text>
-        </Button>
+        <View
+          style={{
+            justifyContent: 'space-around',
+            flexDirection: 'row',
+            paddingTop: 10,
+            paddingHorizontal: 5,
+          }}>
+          <Button onPress={navigate} light={!id}>
+            <Text style={{color: id ? '#fff' : '#ccc'}}>Submit</Text>
+          </Button>
+
+          <Button onPress={randomAsteroidDetails} light={!posts.length}>
+            <Text style={{color: posts.length ? '#fff' : '#ccc'}}>
+              Random Asteroid details
+            </Text>
+          </Button>
+        </View>
       </Content>
     </Container>
   );
